201afb949a449072a72b9a2c87fd9447c2fcdf8e
Leon <leon.lee.ann@gmail.com>
2024-07-17 19:43:11 +0800

    1.8.7 流式输出接口搬到命名空间之外

--- include/leonlog/LeonLog.hpp
+++ include/leonlog/LeonLog.hpp
@@ -5,10 +5,29 @@
 #include <string>
 #include <type_traits>
 
-namespace leon_log {
+// 指针概念
+template<typename T>
+concept AnyPtr =
+	( std::is_pointer_v<std::remove_cvref_t<T>> ||
+	  std::is_null_pointer_v<std::remove_cvref_t<T>> ) &&
+	!( std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, char> ||
+	   std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, char> ) &&
+	!( std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, void> ||
+	   std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, void> );
+
+// 非指针概念
+template<typename T>
+concept NonPtr =
+	std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, T>&&
+	!std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, char>&&
+	!std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, char>&&
+	!std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, void>&&
+	!std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, void>;
 
 using str_t = std::string;
 using oss_t = std::ostringstream;
+
+namespace leon_log {
 
 enum LogLevel_e : int {
 	// 供跟踪、调试时使用的日志, Release编译时不会生成相应代码
@@ -129,28 +148,11 @@ public:
 	LogLevel_e _level;
 };
 
-// 指针概念
-template<typename T>
-concept AnyPtr =
-	( std::is_pointer_v<std::remove_cvref_t<T>> ||
-	  std::is_null_pointer_v<std::remove_cvref_t<T>> ) &&
-	!( std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, char> ||
-	   std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, char> ) &&
-	!( std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, void> ||
-	   std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, void> );
-
-// 非指针概念
-template<typename T>
-concept NonPtr =
-	std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, T>&&
-	!std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, char>&&
-	!std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, char>&&
-	!std::is_same_v<std::remove_pointer_t<std::remove_cvref_t<T>>, void>&&
-	!std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, void>;
+};  // namespace leon_log
 
 // char* 是指针, 但不应该解引输出, 只好单做一个非模板的函数
-inline Log_t& operator<<( Log_t& log_, const char* str_ ) {
-	if( log_._level < g_log_level )
+inline leon_log::Log_t& operator<<( leon_log::Log_t& log_, const char* str_ ) {
+	if( log_._level < leon_log::g_log_level )
 		return log_;
 
 	if( str_ == nullptr )
@@ -161,8 +163,8 @@ inline Log_t& operator<<( Log_t& log_, const char* str_ ) {
 };
 
 // void* 同理
-inline Log_t& operator<<( Log_t& log_, const void* ptr_ ) {
-	if( log_._level < g_log_level )
+inline leon_log::Log_t& operator<<( leon_log::Log_t& log_, const void* ptr_ ) {
+	if( log_._level < leon_log::g_log_level )
 		return log_;
 
 	if( ptr_ == nullptr )
@@ -173,18 +175,18 @@ inline Log_t& operator<<( Log_t& log_, const void* ptr_ ) {
 };
 
 template <NonPtr T>
-inline Log_t& operator<<( Log_t& log_, const T& body_ ) {
+inline leon_log::Log_t& operator<<( leon_log::Log_t& log_, const T& body_ ) {
 
-	if( log_._level >= g_log_level )
+	if( log_._level >= leon_log::g_log_level )
 		static_cast<oss_t&>( log_ ) << body_;
 
 	return log_;
 };
 
 template <AnyPtr T>
-inline Log_t& operator<<( Log_t& log_, T body_ ) {
+inline leon_log::Log_t& operator<<( leon_log::Log_t& log_, T body_ ) {
 
-	if( log_._level < g_log_level )
+	if( log_._level < leon_log::g_log_level )
 		return log_;
 
 	if( body_ == nullptr )
@@ -197,24 +199,22 @@ inline Log_t& operator<<( Log_t& log_, T body_ ) {
 
 #ifdef DEBUG
 
-#define lg_debg g_log_level <= LogLevel_e::Debug && Log_t(LogLevel_e::Debug) << __func__ << "(),"
-#define lg_info g_log_level <= LogLevel_e::Infor && Log_t(LogLevel_e::Infor) << __func__ << "(),"
-#define lg_note g_log_level <= LogLevel_e::Notif && Log_t(LogLevel_e::Notif) << __func__ << "(),"
-#define lg_warn g_log_level <= LogLevel_e::Warnn && Log_t(LogLevel_e::Warnn) << __func__ << "(),"
-#define lg_erro g_log_level <= LogLevel_e::Error && Log_t(LogLevel_e::Error) << __func__ << "(),"
-#define lg_fatl g_log_level <= LogLevel_e::Fatal && Log_t(LogLevel_e::Fatal) << __func__ << "(),"
+#define lg_debg (g_log_level <= LogLevel_e::Debug) && Log_t(LogLevel_e::Debug) << __func__ << "(),"
+#define lg_info (g_log_level <= LogLevel_e::Infor) && Log_t(LogLevel_e::Infor) << __func__ << "(),"
+#define lg_note (g_log_level <= LogLevel_e::Notif) && Log_t(LogLevel_e::Notif) << __func__ << "(),"
+#define lg_warn (g_log_level <= LogLevel_e::Warnn) && Log_t(LogLevel_e::Warnn) << __func__ << "(),"
+#define lg_erro (g_log_level <= LogLevel_e::Error) && Log_t(LogLevel_e::Error) << __func__ << "(),"
+#define lg_fatl (g_log_level <= LogLevel_e::Fatal) && Log_t(LogLevel_e::Fatal) << __func__ << "(),"
 
 #else
 
-#define lg_debg g_log_level <= LogLevel_e::Debug && Log_t(LogLevel_e::Debug)
-#define lg_info g_log_level <= LogLevel_e::Infor && Log_t(LogLevel_e::Infor)
-#define lg_note g_log_level <= LogLevel_e::Notif && Log_t(LogLevel_e::Notif)
-#define lg_warn g_log_level <= LogLevel_e::Warnn && Log_t(LogLevel_e::Warnn)
-#define lg_erro g_log_level <= LogLevel_e::Error && Log_t(LogLevel_e::Error)
-#define lg_fatl g_log_level <= LogLevel_e::Fatal && Log_t(LogLevel_e::Fatal)
+#define lg_debg (g_log_level <= LogLevel_e::Debug) && Log_t(LogLevel_e::Debug)
+#define lg_info (g_log_level <= LogLevel_e::Infor) && Log_t(LogLevel_e::Infor)
+#define lg_note (g_log_level <= LogLevel_e::Notif) && Log_t(LogLevel_e::Notif)
+#define lg_warn (g_log_level <= LogLevel_e::Warnn) && Log_t(LogLevel_e::Warnn)
+#define lg_erro (g_log_level <= LogLevel_e::Error) && Log_t(LogLevel_e::Error)
+#define lg_fatl (g_log_level <= LogLevel_e::Fatal) && Log_t(LogLevel_e::Fatal)
 
 #endif
-
-};  // namespace leon_log
 
 // kate: indent-mode cstyle; indent-width 4; replace-tabs off; tab-width 4;
